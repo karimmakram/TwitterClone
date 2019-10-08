84a7d871aeb4b3dc4746f2751074d8d7be090dda
Karim <karimomakram11@gmail.com>
2019-10-08 17:11:28 +0200

    add table Follow to handel users to follow other

--- app/src/main/java/com/example/karim/twitterclone/DBMS.java
+++ app/src/main/java/com/example/karim/twitterclone/DBMS.java
@@ -10,7 +10,7 @@ public class DBMS {
     private SQLiteDatabase db;
 
     private static final String DB_name = "TwitterClone";
-    private static final int DB_version = 1 ;
+    private static final int DB_version = 3;
     private static final String table_name = "users";
 
 
@@ -18,6 +18,21 @@ public class DBMS {
     public static final String row_name = "name";
     public static final String row_username = "username";
     public static final String row_pass = "password";
+
+    private static final String table_name_follow = "Following";
+
+
+    public static final String row_f_id = "id";
+    public static final String row_f_follower = "Follower";
+    public static final String row_f_follow = "Follow";
+
+
+    private static final String DATABASE_CREATE_follow =
+            "CREATE TABLE IF NOT EXISTS "+table_name_follow+"("+
+                   row_f_id+" integer primary key autoincrement,"+
+                    row_f_follower+" text not null,"+
+                    row_f_follow+" text not null,"+
+                    "FOREIGN KEY ("+row_f_follower+")"+" REFERENCES "+table_name+"( "+row_username+"));";
 
 
     DBMS(Context context){
@@ -62,7 +77,53 @@ public class DBMS {
         return  c ;
     }
 
-    private class CustomSQLiteOpenHalper extends SQLiteOpenHelper {
+
+
+///////////////////////////////////////new table ////////////////////////////////////////////////////////////////
+public Boolean insertFollow(String follower , String follow){
+    String query = "insert into "+table_name_follow
+            +" ("+row_f_follower+"," +row_f_follow + ") "+
+            "values ("+
+            "'"+follower+"', "
+            + "'"+follow + "'" +" );" ;
+    Log.i("INSERT()",query);
+    try {
+        db.execSQL(query);
+        return true ;
+    }
+    catch (Exception e){
+        return false ;
+    }
+
+}
+
+    public Cursor getALLFollowing(){
+        Cursor c = db.rawQuery("Select * From "+table_name_follow,null );
+        return  c ;
+    }
+
+    public Cursor getFollowing(String username){
+        Cursor c = db.rawQuery("Select * From "+table_name_follow +" where "+row_f_follower +"= '"+ username+"'",null );
+        return  c ;
+    }
+
+    public boolean unFollow(String username,String Follow) {
+        try {
+            String qurey = "delete From " + table_name_follow + " where " + row_f_follower + "= '" + username + "' and " + row_f_follow + "= '" + Follow + "';";
+            db.execSQL(qurey);
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+
+
+
+
+        private class CustomSQLiteOpenHalper extends SQLiteOpenHelper {
         public CustomSQLiteOpenHalper(Context c){
             super(c,DB_name,null,DB_version);
         }
@@ -75,11 +136,11 @@ public class DBMS {
                     row_username + " text unique,"+
                     row_pass + " text not null);";
             db.execSQL(new_table);
+            db.execSQL(DATABASE_CREATE_follow);
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
         }
     }
 
